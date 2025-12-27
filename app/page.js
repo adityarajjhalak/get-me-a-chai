7ecb0bcf2ce3d11e52b1f5b2ded835763fba3417
adityarajjhalak <aditya969072370@gmail.com>
2025-12-27 17:56:13 +0530

    designing login page and enhance ui

--- app/page.js
+++ app/page.js
@@ -15,15 +15,27 @@ export default function Home() {
       </div>
     </div>
       <div className="bg-amber-50 h-1 opacity-10">ff</div>
-      <div className="text-white">
-        <h1>Your fans can buy you a chai</h1>
-        <div className="flex gap-5">
-          <div className="">
-            <img src="/wired-lineal-187-suitcase-hover-pinch.svg" width={65} alt=""></img>
-            <p>Fund Yourself</p>
+      <div className="text-white container mx-auto pb-22 pt-14">
+        <h1 className="text-2xl text-center mb-12  font-bold">Your fans can buy you a chai</h1>
+        <div className="flex gap-5 justify-around">
+          <div className="item space-y-1 flex flex-col items-center justify-center">
+            <img className="rounded-full p-2 text-black bg-slate-800" src="/Man-working-on-laptop-icon-illustration-on-transparent-background-PNG.png" width={88} alt=""></img>
+            <p className="font-bold ">Fund Yourself</p>
+             <p className="">Your fans are available for y ou to help you</p>
+          </div>
+          <div className="item space-y-1 flex flex-col items-center justify-center">
+            <img className="rounded-full p-2 text-black bg-slate-800" src="/source (1).gif" width={90} alt=""></img>
+            <p className="font-bold">Fund Yourself</p>
+             <p className="">Your fans are available for y ou to help you</p>
+          </div>
+          <div className="item space-y-1 flex flex-col items-center justify-center">
+            <img className="rounded-full p-2 text-black bg-slate-800" src="/download.png" width={88} alt=""></img>
+            <p className="font-bold">Fans want to help</p>
+           <p className="">Your fans are available for y ou to help you</p>
           </div>
         </div>
       </div>
+      
     </>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,26 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <nav className='flex bg-black text-white justify-between px-4 h-13 items-center'>
-        <div className='logo font-bold'>Get me a chai</div>
-     <ul className='flex justify-between gap-4 '>
+        <div className='logo font-bold flex justify-between items-center'> 
+          <img src='/source.gif' width={50} alt=''/>
+          <span>Get me a chai</span>
+          </div>
+
+     {/*<ul className='flex justify-between gap-4 '>
         <li>Home</li>
         <li>About</li>
         <li>projects</li>
         <li>Sign Up</li>
         <li>Login</li>
-     </ul>
+     </ul>*/}
+     <div>
+      <Link href={"/login"}>
+      <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-lg px-4 py-2.5 text-center leading-5'>Login</button>
+      </Link>
+     </div>
     </nav>
   )
 }
